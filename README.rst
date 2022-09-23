549ac7c99703eae6a89c350cf3e49cfa5fab426c
Andrew Mathas <andrew.mathas@gmail.com>
2022-09-23 13:36:03 +1000

    Initial files

---
+++ README.rst
@@ -0,0 +1,47 @@
+# GradedDimKLR
+
+SageMath_ code for computing the dimensions of the weight spaces of the cyclotomic KLR algebras attached to symmetrisable quivers using the amazinfg forula from the paper "Graded dimensions and monomial bases for the cyclotomic quiver Hecke algebras" of Hu and Shi HuShi_. 
+
+The attached code provides a sigle command `klr_cyclotomic_dimension` that
+computes the ses the Hu-Shi formula to compute the dimension 
+
+Usage
+-----
+
+* With a local installation of SageMath_, start  SageMath_ and attach the file
+`graded_dim_klr` using:
+
+.. code-block:: python
+   sage: %attach graded_dim_klr
+   sage: klr_cyclotomic_dimension(['A',3],[2], [2,3,3,2,1], [2,3,2,3,1])
+   sage: klr_cyclotomic_dimension(['B',3],[2], [2,3,3,2,1], verbose=True)
+
+
+* Using SageMathCell_, cut-and-paste the code from `graded_dim_klr` into a cell
+  and then type the `klr_cyclotomic_dimension` commands into the bottom of the
+  cell.
+
+.. code-block:: python
+   klr_cyclotomic_dimension(['A',3],[2], [2,3,3,2,1], [2,3,2,3,1])
+   klr_cyclotomic_dimension(['B',3],[2], [2,3,3,2,1], verbose=True)
+
+
+Andrew Mathas
+Copyright (C) 2022
+
+------------
+
+GNU General Public License, Version 3, 29 June 2007
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License (GPL_) as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+.. _HuShi: https://arxiv.org/abs/2108.05508G
+.. _SageMath: https://www.sagemath.org/
+.. _SageMathCall: https://sagecell.sagemath.org/
---
+++ graded_dim_klr.py
@@ -0,0 +1,184 @@
+r"""
+    Compute the graded dimension of  e(i)R^Lambda_\alpha e(j),
+    where \Lambda is a dominant weight and i,j\in I^\alpha.
+
+    By Hu-Shi, if i,j\in I^n then
+
+        \dim_q e(i)R^\Lambda_\alpha e(j)
+          = \sum_{w\in\Sym(i,j)}
+              \prod_{t=1}^n[N^\Lambda(w,i,t)}_{i_t}] q_{i_i}^{N^{\Lambda,i,t)-1}}
+
+    sage: R = RootSystem(['G', 2])
+    sage:  klr_cyc_dim(R, [1], [1,2,1,1,2])
+
+
+
+AUTHOR:
+- Andrew Mathas (2022): initial version
+
+"""
+
+from sage.combinat.root_system.cartan_type import CartanType
+from sage.groups.perm_gps.permgroup_named  import SymmetricGroup
+
+
+def quantum_integer(q,k):
+    '''
+    Return the quantum integer (q^k-q^{-k})/(q-q^{-1})
+
+        [1] = 1
+        [2] = q + q^-1
+        [3] = q^2 + 1 + q-2
+        [-k] = -[k]
+
+    '''
+    if k < 0:
+        return -quantum_integer(q, -k)
+
+    return sum( q**(k-2*i-1) for i in range(k))
+
+
+def klr_cyclotomic_dimension(C, L, bi, bj=None, verbose=False):
+    r"""
+    This function implements the Hu-Shi formula for the graded dimension of the
+    weight space e(i) R^L_n e(j), where i,j \in I^n. Here:
+
+        - `C`  is the Cartan type
+        - `L`  is a list specifying the dominant weight
+        - `bi` is a weight in I^n
+        - `bj` is a weight in I^n, which defaults to `bi`
+
+    If `verbose` is set to `True` then some details of the computation are
+    printed as well.
+
+    EXAMPLES::
+
+        sage: klr_cyclotomic_dimension(['D',4],[2], [2,3,4,1], verbose=True)
+        Subgroup of permutations = < () >
+        N(1,t)-1: 0  0  0  0
+        N(w,t):   1  1  1  1
+        X(w): 1
+        1
+        sage: klr_cyclotomic_dimension(['D',4],[2], [2,3,4,1], [2,3,4,1], verbose=True)
+        Subgroup of permutations = < () >
+        N(1,t)-1: 0  0  0  0
+        N(w,t):   1  1  1  1
+        X(w): 1
+        1
+        sage: klr_cyclotomic_dimension(['D',4],[2], [2,3,4,1], [2,4,3,1], verbose=True)
+        Subgroup of permutations = < (1,2) >
+        N(1,t)-1: 0  0  0  0
+        N(w,t):   1  1  1  1
+        X(w): 1
+        1
+        sage: klr_cyclotomic_dimension(['A',2,1],[0], [0,1,2], verbose=True)
+        Subgroup of permutations = < () >
+        N(1,t)-1: 0  0  1
+        N(w,t):   1  1  2
+        X(w): q^2 + 1
+        q^2 + 1
+        sage: klr_cyclotomic_dimension(['A',2,1],[0], [0,2,1], verbose=True)
+        Subgroup of permutations = < () >
+        N(1,t)-1: 0  0  1
+        N(w,t):   1  1  2
+        X(w): q^2 + 1
+        q^2 + 1
+        sage:
+        sage: klr_cyclotomic_dimension(['A',2,1],[0], [0,1,2], [0,2,1], verbose=True)
+        Subgroup of permutations = < (1,2) >
+        N(1,t)-1: 0  0  1
+        N(w,t):   1  1  1
+        X(w): q
+        q
+        sage: klr_cyclotomic_dimension(['A',1],[1,1],[1],[1], verbose=True)
+        Subgroup of permutations = < () >
+        N(1,t)-1: 1
+        N(w,t):   2
+        X(w): q^2 + 1
+        q^2 + 1
+        sage: klr_cyclotomic_dimension(['A',1],[1,1],[1],[1], verbose=True)
+        Subgroup of permutations = < () >
+        N(1,t)-1: 1
+        N(w,t):   2
+        X(w): q^2 + 1
+        q^2 + 1
+        sage: klr_cyclotomic_dimension(['B',3],[2], [2,3,3,2,1], verbose=True)
+        Subgroup of permutations = < (), (1,2), (0,3), (0,3)(1,2) >
+        N(1,t)-1: 0  1 -1  0  1
+        N(w,t):   1  2  0  1  2
+        X(w): 0
+        N(w,t):   1  2  2  1  2
+        X(w): (q^4 + 1)*(q^2 + 1)^2/q^2
+        N(w,t):   1  0 -2  1  2
+        X(w): 0
+        N(w,t):   1  0  0  1  2
+        X(w): 0
+        (q^4 + 1)*(q^2 + 1)^2/q^2
+    """
+    if verbose:
+        def vprint(*args): print(' '.join(f'{a}' for a in args))
+    else:
+        def vprint(*args): pass
+
+    # bj defaults to bi
+    if bj is None:
+        bj = bi
+
+    if len(bi) != len(bj):
+        raise TypeError(f'{bi} and {bj} must have the same length')
+
+    # Convert C to Cartantype
+    try:
+        C = CartanType(C)
+    except TypeError:
+        raise TypeError(f'{C} must be a Cartan type')
+
+    # index set for quiver
+    I = C.index_set()
+    #vprint(f'{I=}')
+
+    if any(i not in I for i in bi):
+        raise TypeError(f'{bi} must in I^n for {I=}')
+
+    if any(j not in I for j in bj):
+        raise TypeError(f'{bj} must in I^n for {I=}')
+
+    # shorthand for Cartan matrix entries
+    Cij = lambda i,j: C.cartan_matrix()[I.index(i), I.index(j)]
+
+    # shorthand for q_di, where is the Cartan symmetriser
+    qdi  = lambda i: q**(C.symmetrizer()[i])
+
+    # work in SymmetricGroup(n)
+    n = len(bi)
+
+    q = var('q')
+
+    N = 0 # will become \sum_w \prod_t [N^L(w,bi,t)]q^{N^L(1,bi,t)-1}
+
+    Nwt = lambda w,t: L.count(bi[t]) - sum(Cij(bi[t], bi[j]) for j in range(t) if w(j)<w(t))
+    Sn = SymmetricGroup(range(n))
+    # find the subgroup of Sn that maps bi to bj
+    generators = []
+    positions={}
+    for w in Sn:
+        if all( bj[w(i)]==bi[i] for i in range(n) ):
+            generators.append(w)
+    vprint(f'Subgroup of permutations = {generators}'.replace('[', '< ').replace(']',' >'))
+    # first calculate NOne[t] = Nwt(1, t) for t in range(n)
+    NOne = [ Nwt(Sn.one(), t)-1 for t in range(n)]
+    vprint(f'N(1,t)-1:{" ".join(f"{i:>2}" for i in NOne)}')
+    #vprint('qdi:   ',' '.join(f'{qdi(bi[t])}' for t in range(n)))
+    for w in Sn.subgroup( generators ):
+        # Sym(i,j) = { w\in Sym_n | wi = j}
+        if all( bj[w(i)]==bi[i] for i in range(n) ): # if w(bi) == bj
+            vprint(f'N(w,t): ',' '.join(f'{Nwt(w,t):>2}' for t in range(n)))
+            Nw = prod([
+                  quantum_integer( qdi(bi[t]), Nwt(w,t) )*( qdi(bi[t])**(NOne[t]) )
+                  for t in range(n)
+            ])
+            vprint(f'X(w):', Nw.factor())
+            N += Nw
+    return N.factor()
+
+
